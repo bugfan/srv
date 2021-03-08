c717b2bcad0a06df3134b717249f5795e1b81edb
bugfan <908958194@qq.com>
2021-03-08 18:22:37 +0800

    fuck

--- srv.go
+++ srv.go
@@ -1,35 +1,104 @@
 package srv
 
 import (
+	"crypto/tls"
 	"net"
 	"net/http"
-
-	"github.com/soheilhy/cmux"
+	"reflect"
 )
 
-type Server struct {
-	handlers []http.Handler
+type GMux interface {
+	AppendHandler(handlers ...interface{})
+	Serve() error
+	Close()
 }
 
-func New(listen string) (interface{}, error) {
-	// 单端口复用
+type Matcher interface {
+	Match(*http.Request) bool
+}
+type Handler interface {
+	ServeHTP(http.ResponseWriter, *http.Request)
+}
 
-	l, err := net.Listen("tcp", listen)
+type Server struct {
+	instances []interface{}
+	proxys    []Proxy
+	addr      string
+	listener  net.Listener
+}
+type Proxy interface {
+	Matcher
+}
+
+func (s *Server) AppendHandler(ifs ...interface{}) {
+	for _, i := range ifs {
+		reflectVal := reflect.ValueOf(i)
+		t := reflect.Indirect(reflectVal).Type()
+		newObj := reflect.New(t)
+		handler, ok := newObj.Interface().(Handler)
+		if ok {
+			matcher, ok := newObj.Interface().(Matcher)
+			if ok {
+
+			}
+			s.proxys = append(s.proxys, Proxy)
+		}
+	}
+
+}
+func (s *Server) Close() {
+	s.Close()
+}
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	for _, h := range s.handlers {
+		h.ServeHTTP(w, r)
+	}
+}
+func (s *Server) Serve() error {
+	server := &http.Server{
+		Handler: s,
+		Addr:    s.addr,
+	}
+	return server.Serve(s.listener)
+}
+
+func New(addr string) (GMux, error) {
+	l, err := net.Listen("tcp", addr)
 	if err != nil {
 		return nil, err
 	}
-	m := cmux.New(l)
-	// tlsL := m.Match(cmux.TLS())
-	// http2 := m.Match(cmux.HTTP2())
-	httpL := m.Match(cmux.HTTP1Fast())
-	go p.GetHTTPServer(listen).Serve(httpL)
-	// go p.GetHTTPSServer(listen).ServeTLS(http2, "", "")
-	// go p.GetHTTPSServer(listen).ServeTLS(tlsL, "", "")
-	return m, nil
+	s := &Server{
+		addr:     addr,
+		listener: l,
+	}
+	return s, nil
 }
 
-func (s *Server) AppendServer(handlers ...http.Handler) {
-	for _, h := range handlers {
-		s.handlers = append(s.handlers, h)
+func getTLSConfig() *tls.Config {
+	tlsconf := &tls.Config{
+		// GetCertificate: p.GetCertificate,
+		// NextProtos: []string{ACMETLS1Protocol},
 	}
+	tlsconf.InsecureSkipVerify = true
+
+	tlsconf.PreferServerCipherSuites = true
+
+	tlsconf.CipherSuites = []uint16{
+		//tls.TLS_AES_128_GCM_SHA256,
+		//tls.TLS_CHACHA20_POLY1305_SHA256,
+		//tls.TLS_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+	}
+	return tlsconf
 }
--- srv_test.go
+++ srv_test.go
@@ -2,11 +2,22 @@ package srv
 
 import (
 	"fmt"
+	"net/http"
 	"testing"
 )
 
 func TestSrv(*testing.T) {
 	s, err := New(":8080")
-	fmt.Println("err:", err)
-	s.AppendServer()
+	fmt.Println("err:", err, s)
+	s.AppendHandler(&t1{})
+	http.ListenAndServe()
+	s.Serve()
+}
+
+type t1 struct {
+}
+
+func (t *t1) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("in-->:", r.URL.String())
+	fmt.Fprint(w, "t1")
 }
