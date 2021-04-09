444f68b8a6afdbba7e467169b17e77e660bc3a2d
bugfan <908958194@qq.com>
2021-04-09 15:34:53 +0800

    srv v1

--- srv.go
+++ srv.go
@@ -2,103 +2,102 @@ package srv
 
 import (
 	"crypto/tls"
+	"log"
 	"net"
 	"net/http"
-	"reflect"
 )
 
-type GMux interface {
-	AppendHandler(handlers ...interface{})
-	Serve() error
-	Close()
-}
-
-type Matcher interface {
-	Match(*http.Request) bool
-}
-type Handler interface {
-	ServeHTP(http.ResponseWriter, *http.Request)
-}
-
 type Server struct {
-	instances []interface{}
-	proxys    []Proxy
-	addr      string
-	listener  net.Listener
-}
-type Proxy interface {
-	Matcher
+	serveMux       *http.ServeMux
+	headMiddleware []http.Handler
+	tailMiddleware []http.Handler
+	addr           string
+	tls            *tls.Config
 }
 
-func (s *Server) AppendHandler(ifs ...interface{}) {
-	for _, i := range ifs {
-		reflectVal := reflect.ValueOf(i)
-		t := reflect.Indirect(reflectVal).Type()
-		newObj := reflect.New(t)
-		handler, ok := newObj.Interface().(Handler)
-		if ok {
-			matcher, ok := newObj.Interface().(Matcher)
-			if ok {
-
-			}
-			s.proxys = append(s.proxys, Proxy)
-		}
-	}
-
-}
-func (s *Server) Close() {
-	s.Close()
-}
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	for _, h := range s.handlers {
-		h.ServeHTTP(w, r)
-	}
-}
-func (s *Server) Serve() error {
-	server := &http.Server{
-		Handler: s,
-		Addr:    s.addr,
-	}
-	return server.Serve(s.listener)
-}
-
-func New(addr string) (GMux, error) {
-	l, err := net.Listen("tcp", addr)
-	if err != nil {
-		return nil, err
-	}
+func New(addr string) *Server {
 	s := &Server{
+		serveMux: http.NewServeMux(),
 		addr:     addr,
-		listener: l,
 	}
-	return s, nil
+	return s
 }
 
-func getTLSConfig() *tls.Config {
-	tlsconf := &tls.Config{
-		// GetCertificate: p.GetCertificate,
-		// NextProtos: []string{ACMETLS1Protocol},
+func (s *Server) SetTLSConfig(tls *tls.Config) {
+	if tls != nil {
+		s.tls = tls
 	}
-	tlsconf.InsecureSkipVerify = true
+}
 
-	tlsconf.PreferServerCipherSuites = true
-
-	tlsconf.CipherSuites = []uint16{
-		//tls.TLS_AES_128_GCM_SHA256,
-		//tls.TLS_CHACHA20_POLY1305_SHA256,
-		//tls.TLS_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+func (s *Server) SetTLSConfigFromBytes(certPEM, keyPEM []byte) {
+	cert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		log.Println("SetTLSConfigFromBytes error:", err)
+		return
 	}
-	return tlsconf
+
+	s.tls = &tls.Config{
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+		},
+		// MinVersion:               tls.VersionTLS12,
+		// MaxVersion:               tls.VersionTLS12,
+		PreferServerCipherSuites: true,
+		InsecureSkipVerify:       true,
+		Certificates:             []tls.Certificate{cert},
+	}
+}
+
+func (s *Server) AddHeadHandler(handlers ...http.Handler) {
+	s.headMiddleware = append(s.headMiddleware, handlers...)
+}
+func (s *Server) AddTailHandler(handlers ...http.Handler) {
+	s.tailMiddleware = append(s.tailMiddleware, handlers...)
+}
+
+func (s *Server) Handle(pattern string, handler http.Handler) {
+	s.serveMux.Handle(pattern, s.wrap(handler))
+}
+
+func (s *Server) wrap(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for _, m := range s.headMiddleware {
+			m.ServeHTTP(w, r) // serve
+		}
+		h.ServeHTTP(w, r)
+		for _, m := range s.tailMiddleware {
+			m.ServeHTTP(w, r)
+		}
+	})
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.serveMux.ServeHTTP(w, r)
+}
+
+func (s *Server) Run() error {
+	log.Printf("Listen at %s\n", s.addr)
+	if s.tls != nil {
+		server := &http.Server{
+			Handler:   s,
+			TLSConfig: s.tls,
+		}
+		l, err := net.Listen("tcp", s.addr)
+		if err != nil {
+			return err
+		}
+		return server.Serve(l)
+	}
+	return http.ListenAndServe(s.addr, s)
 }
--- srv_test.go
+++ srv_test.go
@@ -7,17 +7,85 @@ import (
 )
 
 func TestSrv(*testing.T) {
-	s, err := New(":8080")
-	fmt.Println("err:", err, s)
-	s.AppendHandler(&t1{})
-	http.ListenAndServe()
-	s.Serve()
+	addr := ":8080"
+	// new server
+	server := New(addr)
+
+	// set head middleware
+	server.AddHeadHandler(&zlog{}, &auth{})
+
+	// set tail middleware
+	server.AddTailHandler(&zlog{})
+
+	// set your handler
+	server.Handle("/", &yourHandler{})
+	server.Handle("/ws/", &yourWebsocketHandler{})
+	server.Handle("/static/", &yourStaticHandler{})
+
+	/*
+	* if hava tls certificate data
+	 */
+	// keyData := []byte("xxxx")
+	// certData := []byte("xxxx")
+	// server.SetTLSConfigFromBytes(certData, keyData)
+
+	/*
+	* if hava tls config
+	 */
+	// tlsconfig := &tls.Config{}
+	// server.SetTLSConfig(tlsconfig)
+
+	// listen server
+	/*
+	* method 1
+	* run directly
+	 */
+	server.Run()
+
+	/*
+	* method 2
+	* if you have own listener
+	 */
+	// http.ListenAndServe(addr, server)
 }
 
-type t1 struct {
+type yourHandler struct {
 }
 
-func (t *t1) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("in-->:", r.URL.String())
-	fmt.Fprint(w, "t1")
+func (s *yourHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("your handler:", r.URL.String())
+	fmt.Fprint(w, "your handler")
+}
+
+type yourWebsocketHandler struct {
+}
+
+func (s *yourWebsocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("your websocket handler:", r.URL.String())
+	fmt.Fprint(w, "your websocket handler")
+}
+
+type yourStaticHandler struct {
+}
+
+func (s *yourStaticHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("your static handler:", r.URL.String())
+	fmt.Fprint(w, "your static handler")
+}
+
+// log middleware
+type zlog struct {
+}
+
+func (s *zlog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("log handle:", r.URL.String())
+}
+
+// auth middleware
+type auth struct {
+}
+
+func (s *auth) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// todo: auth code
+	fmt.Println("auth handle", r.URL.String())
 }
